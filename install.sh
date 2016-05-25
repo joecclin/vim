8a4588b5643d333c12525bed4b2c64f8c932ccf2
joecclin <josh.lin@axiomtek.com.tw>
2016-05-25 13:29:05 +0800

    1. add config setting in the install script

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# for user name in one git
+# for one user name in one git
 # git config user.name "joecclin"
+
+# for git config to push all branches
+git config --global push.default matching
+# for git config to push this working branches
+#git config --global push.default simple
+
 
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
