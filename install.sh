319a7e87236c5a765f059e288c0f7afd79dad0c1
joecclin <josh.lin@axiomtek.com.tw>
2016-05-25 13:25:02 +0800

    1. add install script for config vimdiff

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# for user name in one git
+# git config user.name "joecclin"
+
+git config --global diff.tool vimdiff
+git config --global difftool.prompt false
+git config --global alias.d difftool
